e927237b96fe3077ee6d44b8f59c4177868e6cec
吃吃 <jasony12y@domodomotekiMac.local>
2017-10-24 09:58:38 -0700

    Initial Commit

---
+++ IOS第一次作業/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS第一次作業
+//
+//  Created by 吃吃 on 2017/10/24.
+//  Copyright © 2017年 就是要吃！. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
